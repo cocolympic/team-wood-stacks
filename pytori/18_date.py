c20876ba74772845add2600469382d57363e3f5e
khjasonlam <khjasonlam@gmail.com>
2025-09-04 09:06:08 +0900

    Add 18_date.py

---
+++ pytori/18_date.py
@@ -0,0 +1,12 @@
+import datetime
+
+print(datetime.date(2025, 1, 1))
+print(datetime.date(2025, 1, 2))
+print(datetime.date(2025, 1, 3))
+print(datetime.date(2025, 1, 4))
+print(datetime.date(2025, 1, 5))
+print(datetime.date(2025, 1, 6))
+print(datetime.date(2025, 1, 7))
+print(datetime.date(2025, 1, 8))
+print(datetime.date(2025, 1, 9))
+print(datetime.date(2025, 1, 10))
