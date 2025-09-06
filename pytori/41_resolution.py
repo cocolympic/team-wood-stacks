11613e03b645ac7ec62bafb6bf8aa8bd4f7e6d1b
khjasonlam <khjasonlam@gmail.com>
2025-09-06 11:08:06 +0900

    Add 41_resolution.py

---
+++ pytori/41_resolution.py
@@ -0,0 +1,10 @@
+import datetime
+
+print(datetime.datetime.now().resolution)
+print(datetime.date.resolution)
+print(datetime.time.resolution)
+print(datetime.timedelta.resolution)
+print(datetime.datetime.resolution)
+print(datetime.date.resolution)
+print(datetime.time.resolution)
+print(datetime.timedelta.resolution)
