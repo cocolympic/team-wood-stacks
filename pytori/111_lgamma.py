2885987b6ddeed07da82cc80c19701cf0d03dcea
khjasonlam <khjasonlam@gmail.com>
2025-09-08 22:47:29 +0900

    Add 111_lgamma.py

---
+++ pytori/111_lgamma.py
@@ -0,0 +1,6 @@
+import math
+
+print(math.lgamma(1))    # ln(Γ(1)) = ln(1) = 0.0
+print(math.lgamma(2))    # ln(Γ(2)) = ln(1) = 0.0
+print(math.lgamma(3))    # ln(Γ(3)) = ln(2) ≈ 0.693
+print(math.lgamma(5))    # ln(Γ(5)) = ln(24) ≈ 3.178
